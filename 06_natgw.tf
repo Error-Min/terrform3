814f0f91be986ce0952a3a9f518302f2087f5fa2
errormin <smgm030@gmail.com>
2021-11-25 16:57:03 +0900

    step1

---
+++ 06_natgw.tf
@@ -0,0 +1,10 @@
+#natgw에쓸(할당할) eip
+resource "aws_eip" "sdkim_naw_ip" {
+  vpc = true
+}
+
+
+resource "aws_nat_gateway" "sm-ngw" {
+  allocation_id = aws_eip.sdkim_naw_ip.id
+  subnet_id = aws_subnet.sm_pria.id
+}
