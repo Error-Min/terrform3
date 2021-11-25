abdae6b5add17c5d0f670e6898242cfa3f02d936
errormin <smgm030@gmail.com>
2021-11-25 16:57:55 +0900

    step1

--- 06_natgw.tf
+++ 06_natgw.tf
@@ -6,5 +6,5 @@ resource "aws_eip" "sdkim_naw_ip" {
 
 resource "aws_nat_gateway" "sm-ngw" {
   allocation_id = aws_eip.sdkim_naw_ip.id
-  subnet_id = aws_subnet.sm_pria.id
+  subnet_id = aws_subnet.sm_puba.id
 }
