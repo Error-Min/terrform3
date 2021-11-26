f866fcbb2d0108433c5278be5c9ef4264fc8b1e7
errormin <smgm030@gmail.com>
2021-11-26 10:38:31 +0900

    step1

--- 07_ngwrt.tf
+++ 07_ngwrt.tf
@@ -3,8 +3,8 @@ resource "aws_route_table" "sm_ngwrt" {
   vpc_id = aws_vpc.sm_vpc.id
 
   route {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = aws_nat_gateway.sm-ngw.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.sm-ngw.id
   }
 
   tags = {
